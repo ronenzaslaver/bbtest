852a69f34ca0befe3e9e3634d7e2a2c06b3ae334
Yoram Shamir <yoram@ignissoft.com>
2019-01-16 13:34:49 +0200

    WIP with Linn

--- tests/test_os_agnostic.py
+++ tests/test_os_agnostic.py
@@ -1,11 +1,13 @@
 
+import pytest
+
 import subprocess
 
 from bbtest import BBTestCase
 from .mytodo_box import MyToDoBox
 
 
-class BaseToDoTest(BBTestCase):
+class TestOsAgnostic(BBTestCase):
 
     LAB = {
         'host1': {
@@ -16,14 +18,27 @@ class BaseToDoTest(BBTestCase):
 
     address_book = {'host1': {'ip': None, 'auth': None}}
 
-    def test_operations(self):
+    @classmethod
+    def setup_class(cls):
+        print('in setup_class')
+
+    def setup(self):
+        print('in setup')
+
+    def test_operations(self, os):
+        print('in test')
+        return
+        # Test no output.
         box = self.lab.boxes[MyToDoBox.NAME][0]
-        self.assertEqual(box.do_nothing(), [])
+        assert box.do_nothing() == []
+        # Test output capture.
         box.add('Foo')
         todos = box.list()
-        self.assertEqual(len(todos), 1)
-        self.assertEqual(todos[0], 'Foo')
+        assert len(todos) == 1
+        assert todos[0] == 'Foo'
         box.delete('Foo')
         todos = box.list()
-        self.assertEqual(len(todos), 0)
-        self.assertRaises(subprocess.SubprocessError, box.add, '')
+        # Test error.
+        assert len(todos) == 0
+        with pytest.raises(subprocess.SubprocessError) as _:
+            box.add('')
